1d14681a87d8273c38793cf2571a8a44768603bf
Vadim <bad-us@rambler.ru>
2020-12-09 14:30:59 +0300

    Lesson 3

--- Task_3.py
+++ Task_3.py
@@ -1,11 +1,20 @@
-'''3. По введенным пользователем координатам двух точек вывести уравнение
-прямой вида y=kx+b, проходящей через эти точки.'''
+'''3. В массиве случайных целых чисел поменять местами минимальный и
+максимальный элементы.'''
 
-x1, y1, x2, y2 = [
-    int(x) for x in input('Введите кординаты (x1 y1 x2 y2): ').split()
-]
+import random
 
-k = (y2 - y1)/(x2 - x1)
-b = y1 - k * x1
+r = [random.randint(0, 99) for _ in range(10)]
+print(f'Массив до изменения: {r}')
 
-print(f'Уравнение прямой: y = {k}x + {b}')
+max = r[0]
+min = r[0]
+
+for i in r:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+min_index = r.index(min)
+max_index = r.index(max)
+r[min_index], r[max_index] = r[max_index], r[min_index]
+print(f'Массив после изменения элементов {min_index} и {max_index}: {r}')
--- Task_5.py
+++ Task_5.py
@@ -1,26 +1,19 @@
-'''5. Пользователь вводит две буквы. Определить, на каких местах алфавита они
-стоят и сколько между ними находится букв.'''
+'''5. В массиве найти максимальный отрицательный элемент. Вывести на
+экран его значение и позицию в массиве.'''
 
-letter1, letter2 = [
-  x.upper() for x in input('Введите 2 буквы от A до Z через пробел: ').split()
-]
+import random
 
-# Генерация списка букв
-abc_list = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+r = [random.randint(-99, 99) for _ in range(100)]
+print(f'Массив: {r}')
 
-index_letter1 = abc_list.index(letter1) + 1
-index_letter2 = abc_list.index(letter2) + 1
+min_index = 0
 
-if index_letter1 < index_letter2:
-    step = 1
+for i in r:
+    if r[min_index] > i:
+        min_index = r.index(i)
+
+if r[min_index] >= 0:
+    print(f'В массиве нет отрицательных элементов')
 else:
-    step = -1
-
-print(f'Первая буква {letter1}, в алфавите под номером: {index_letter1}')
-print(f'Вторая буква {letter2}, в алфавите под номером: {index_letter2}')
-
-print(
-    f'Между ними находятся  \
-{abc_list[abc_list.index(letter1) + step:abc_list.index(letter2):step]} \
-({abs(index_letter1 - index_letter2) - 1})'
-        )
+    print(f'В массиве минимальный отрицательный элемент: {r[min_index]}.',
+            f'Находится в массиве на позиции {min_index}')
